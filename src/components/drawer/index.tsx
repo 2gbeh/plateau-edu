085e0297446610d29868f118059dec7f56e744fe
etugbeh <emmanuel@mycardio.io>
2024-05-17 21:34:28 +0100

    update drawer

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
-import { CircleUser, Menu, Package2, Search } from "lucide-react";
+import clsx from "clsx";
+import { Home, ContactRound, Users, Menu } from "lucide-react";
 import { Button } from "@/components/shadcn/ui/button";
 import {
   Sheet,
@@ -7,56 +10,59 @@ import {
   SheetTrigger,
 } from "@/components/shadcn/ui/sheet";
 import { Figure } from "../figure";
+import { useActiveLink } from "@/hooks/useActiveLink";
+//
+import APP from "@/constants/APP";
+import { menu } from "./drawer.util";
 
 export const Drawer = () => {
+  const { isActiveLink } = useActiveLink();
+  //
   return (
     <Sheet>
       <SheetTrigger asChild>
         <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-          <Figure
-            src="/icon.png"
-            dim={[256, 256]}
-            fit="40px"
-            alt="Toggle navigation menu"
-          />
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle navigation menu</span>
         </Button>
       </SheetTrigger>
       <SheetContent side="left">
         <nav className="grid gap-6 text-lg font-medium">
           <Link
-            href="#"
+            href="/"
             className="flex items-center gap-2 text-lg font-semibold"
           >
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
+            <Figure
+              src="/icon.png"
+              dim={[256, 256]}
+              fit="40px"
+              alt={APP.name}
+            />
           </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground hover:text-foreground"
-          >
-            Dashboard
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground hover:text-foreground"
-          >
-            Orders
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground hover:text-foreground"
-          >
-            Products
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground hover:text-foreground"
-          >
-            Customers
-          </Link>
-          <Link href="#" className="hover:text-foreground">
-            Settings
-          </Link>
+          {/*  */}
+          {menu.map((e, i) => {
+            return (
+              <Link
+                key={i}
+                href={e.path}
+                className={clsx(
+                  "flex-center",
+                  isActiveLink(e.path)
+                    ? "hover:text-foreground"
+                    : "text-muted-foreground hover:text-foreground"
+                )}
+              >
+                {
+                  {
+                    home: <Home className="dim-5 mr-2" />,
+                    contact_round: <ContactRound className="dim-5 mr-2" />,
+                    users: <Users className="dim-5 mr-2" />,
+                  }[e.icon]
+                }
+                <span>{e.text}</span>
+              </Link>
+            );
+          })}
         </nav>
       </SheetContent>
     </Sheet>
